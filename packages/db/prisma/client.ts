6d0a953716fbefcb909d26aa80e87d748f4e5ca6
Ramkarthik <ramkarthikkrishnamurthy@gmail.com>
2025-12-14 03:12:02 +0530

    Adapter for Prisma

--- packages/db/prisma/client.ts
+++ packages/db/prisma/client.ts
@@ -1,6 +1,12 @@
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { PrismaClient } from './generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
 
 export const prisma = new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL!,
+    //accelerateUrl: process.env.DATABASE_URL!,
+    adapter,
 }).$extends(withAccelerate())
